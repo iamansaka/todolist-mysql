48aec40acfb98e41ffe5c427b7a261373f65f859
iamansaka <anssaka.mahamoudou@icloud.com>
2021-09-02 20:43:31 +0200

    Modification fichier css, index, edit-todo

--- actions/edit-todo.php
+++ actions/edit-todo.php
@@ -5,13 +5,14 @@ require_once "./database.php";
 if ($_SERVER['REQUEST_METHOD'] === "GET") {
     if (!empty($_GET['id']) && !empty($_GET['done'])) {
         $idTodo = htmlentities($_GET['id']);
-        $doneTodo = (int)htmlentities($_GET['done']);
-        $doneTodo !== $doneTodo;
+        $done = htmlentities($_GET['done']);
 
-        $statementEdit = $pdo->prepare("UPDATE todo_list SET done = :done WHERE id = :id");
-        $statementEdit->bindValue(":id", $idTodo, PDO::PARAM_INT);
-        $statementEdit->bindValue(":done", $doneTodo, PDO::PARAM_INT);
-        $statementEdit->execute();
+        if ($done == !$done) {
+            $statementEdit = $pdo->prepare("UPDATE todo_list SET done = :done WHERE id = :id");
+            $statementEdit->bindValue(":done", $done, PDO::PARAM_INT);
+            $statementEdit->bindValue(":id", $idTodo, PDO::PARAM_INT);
+            $statementEdit->execute();
+        }
     }
 }
 
